ee6a09b3f73a5d5944556229532170293903644b
Isabella-Amantino <isabella.amantino@hotmail.com>
2022-10-05 14:47:59 -0300

    Backend - Task 3

---
+++ projetoint/src/main/java/com/daterra/projetoint/model/Produtos.java
@@ -0,0 +1,97 @@
+package com.daterra.projetoint.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produtos {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(min = 3, max = 255,message = "O atributo nome deve ter no máximo 255 caracteres")
+	private String nome;
+	
+	@NotBlank
+	@Size(min = 3, max = 1000,message = "O atributo descrição deve ter no máximo 1000 caracteres")
+	private String descricao;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)// responsável por fazer a formatação do preço. Arredonda quando enviado por front
+	@NotNull(message = "Preço é obrigatório!")
+	@Positive(message = "O preço deve ser maior do que zero!")//Não deixa cadastrar número negativo ou zerado.
+	private BigDecimal preco;
+
+	
+	private String foto;
+	
+	@NotNull
+	private Long quantidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Long quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+	
+	
+	
+	
+}
