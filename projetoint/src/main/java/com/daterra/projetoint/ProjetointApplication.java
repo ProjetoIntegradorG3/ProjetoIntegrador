fdafdc2ffad90486da93f737847dc7ee6b463dcf
Isabella-Amantino <isabella.amantino@hotmail.com>
2022-10-05 10:30:59 -0300

    Task 3

---
+++ projetoint/src/main/java/com/daterra/projetoint/ProjetointApplication.java
@@ -0,0 +1,13 @@
+package com.daterra.projetoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetointApplication.class, args);
+	}
+
+}
